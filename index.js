d8ada0095cb141f80bdafc68cc44b2df4071ba75
rimasultana <rimasultana20225@gmail.com>
2024-03-06 19:37:38 +0600

    maps delete

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const fruits = new Map();
-
-// Set Map Values
-fruits.set("apples", 500);
-fruits.set("bananas", 300);
-fruits.set("oranges", 200);
-
-document.getElementById("demo").innerHTML = fruits.get("banana");
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+  
+  // Delete an Element
+  fruits.delete("apples");
+  
+  document.getElementById("demo").innerHTML = fruits.size;
